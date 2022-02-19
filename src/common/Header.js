932bbb6729f5a8e41d36fde139d93933c81b9d89
Vinicius Depizzol <vdepizzol@gmail.com>
2022-02-19 12:44:42 -0800

    copy

--- src/common/Header.js
+++ src/common/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render() {
       return (
         <div id="header">
-          <h1>Grid-friendly relative line heights for the web</h1>
+          <h1>Grid-friendly type scale</h1>
           <p>Generate <code>rem</code> font sizes and <code>unitless</code> line heights while thinking of your type scale in pixels (like modern software design tools do). <a href="#">Learn more</a>.</p>
         </div>
       );
